36168f93538487fe0dfee7126e0be4eb3cf23473
HDS20 <46461888+HDS20@users.noreply.github.com>
2019-01-28 09:09:43 +0100

    Add files via upload

--- createdataset.R
+++ createdataset.R
@@ -1,13 +1,14 @@
 # function that create random dataset consists of 4 variables
-# version 1.2 created 2019.01.08
+# version 1.3 created 2019.01.28
 # x1 - 1'st variable
 # x2 - 2'st variable correlated with x1 at level1
 # z1  - (0,1) propobility of dependent event
-# zz1 - binary dependent variable with occurences of event 1 eq. 50%
+# zz1 - binary dependent variable with occurrences of event 1 eq. 50%
+require(stats)
 
-createdataset<-function(size,level1,level2,w1,w2){
+createdataset<-function(size,level1,level2,w1,w2,ocr=NULL){
 
-  
+  set.seed(456321, kind = NULL, normal.kind = NULL)
   ERR=0
   if ((size)<1) {ERR=1}
   if (abs(level1)>1) {ERR=2}
@@ -29,17 +30,27 @@ createdataset<-function(size,level1,level2,w1,w2){
   x2<<-sign(level1)*(level1^2)*x1+(1-(level1^2))*rnorm(size)
   
   #noise 
-  noise<-rnorm(size)
+  noise<<-rnorm(size)
   
-  w<-array(3)
-  w[1]<-w1
-  w[2]<-w2
+  
+  
+  #w1<-w1/(var(x1)^0.5*w1+var(x2)^0.5*w2+var(noise)^0.5*level2)
+  #w2<-w2/(var(x1)^0.5*w1+var(x2)^0.5*w2+var(noise)^0.5*level2)
+  #level2<-level2/(var(x1)^0.5*w1+var(x2)^0.5*w2+var(noise)^0.5*level2)
+  
+  w1<-w1/(w1+w2+level2)
+  w2<-w2/(w1+w2+level2)
+  level2<-level2/(w1+w2+level2)
+  
+  suma<<-w1*x1+w2*x2+level2*noise
   
   #dependent variable (Probability)
-  z1<<-1/(1+exp((w[1]*x1+w[2]*x2+level2*noise)))
+  z1<<-1/(1+exp((suma)))
   
-  #binary dependent variable with 50% occurences of 1
+  #binary dependent variable with 50% occurrences of 1
   zz1<<-ifelse(z1 > median(z1),1,0)
+  #binary dependent variable with ocr occurrences of 1
+  #zz1<-ifelse(z1 < quantile(z1,ocr),1,0)
   
 }
 
